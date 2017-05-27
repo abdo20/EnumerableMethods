35890ca1b6f7b1333e32484461cecedc69d663b4
abdo20 <abdelrhmanadel93@gmail.com>
2017-05-27 03:01:38 +0200

    added my_inject method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,26 +1,28 @@
 module Enumerable
 	def my_each(list)
 		# take a list and perform a certain action on each item in the list
-		#for i in list
-		#	yield(i)
-		#end
-		curent_index=0
-		while curent_index<list.length do
-			yield(list[curent_index])
-			curent_index+=1
+		for i in list
+			yield(i)
 		end
+		# anther way using while loop
+		#curent_index=0
+		#while curent_index<list.length do
+		#	yield(list[curent_index])
+		#	curent_index+=1
+		#end
 	end
 
 	def my_each_with_index(list)
 		# take a list and perform a certain action on each item and its index
-		#for i in 0...list.length
-		#	yield(list[i],i)
-		#end
-		curent_index=0
-		while curent_index<list.length do
-			yield(list[curent_index], curent_index)
-			curent_index+=1
+		for i in 0...list.length
+			yield(list[i],i)
 		end
+		# anther way using while loop
+		#curent_index=0
+		#while curent_index<list.length do
+		#	yield(list[curent_index], curent_index)
+		#	curent_index+=1
+		#end
 	end
 
 	def my_select(list)
@@ -75,6 +77,24 @@ module Enumerable
 			end
 		end
 		return count
+	end
+
+	def my_map(list)
+		#return a new list of all items after get passed in the block
+		new_list=Array.new
+		my_each(list) do |i|
+			new_list<<yield(i)
+		end
+		return new_list
+	end
+
+	def my_inject(list)
+		#return a sum value for all the items values after passing the block
+		total=0
+		my_each(list) do |i|
+			total+=yield(total,i)-total
+		end
+		return total
 	end
 end
 
